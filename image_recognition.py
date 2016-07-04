8067a67998273996d086f715748b2d7f09790bb0
Takashi Ogura <t.ogura@gmail.com>
2016-07-04 23:00:00 +0900

    Use direct jpg data instead of jpg file
    add some params

--- image_recognition.py
+++ image_recognition.py
@@ -1,41 +1,43 @@
 import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
-from cv_bridge import CvBridge, CvBridgeError
+from cv_bridge import CvBridge
 import cv2
 import numpy as np
 import tensorflow as tf
 from tensorflow.models.image.imagenet import classify_image
 
+
 class RosTensorFlow():
     def __init__(self):
         classify_image.maybe_download_and_extract()
+        self._session = tf.Session()
+        classify_image.create_graph()
+        self._cv_bridge = CvBridge()
+
         self._sub = rospy.Subscriber('image', Image, self.callback, queue_size=1)
         self._pub = rospy.Publisher('result', String, queue_size=1)
+        self.score_threshold = rospy.get_param('~score_threshold', 0.1)
+        self.use_top_k = rospy.get_param('~use_top_k', 5)
 
     def callback(self, image_msg):
-        bridge = CvBridge()
-        print 'start'
-        cv_image = bridge.imgmsg_to_cv2(image_msg, "bgr8")
-        cv2.imwrite('tmp.jpg', cv_image)
+        cv_image = self._cv_bridge.imgmsg_to_cv2(image_msg, "bgr8")
         # copy from
         # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/models/image/imagenet/classify_image.py
-        image_data = tf.gfile.FastGFile('tmp.jpg', 'rb').read()
+        image_data = cv2.imencode('.jpg', cv_image)[1].tostring()
         # Creates graph from saved GraphDef.
-        classify_image.create_graph()
-        with tf.Session() as sess:
-            softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
-            predictions = sess.run(softmax_tensor,
-                                   {'DecodeJpeg/contents:0': image_data})
-            predictions = np.squeeze(predictions)
-            # Creates node ID --> English string lookup.
-            node_lookup = classify_image.NodeLookup()
-            num_top_predictions = 5
-            top_k = predictions.argsort()[-num_top_predictions:][::-1]
-            for node_id in top_k:
-                human_string = node_lookup.id_to_string(node_id)
-                score = predictions[node_id]
-                print('%s (score = %.5f)' % (human_string, score))
+        softmax_tensor = self._session.graph.get_tensor_by_name('softmax:0')
+        predictions = self._session.run(
+            softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+        # Creates node ID --> English string lookup.
+        node_lookup = classify_image.NodeLookup()
+        top_k = predictions.argsort()[-self.use_top_k:][::-1]
+        for node_id in top_k:
+            human_string = node_lookup.id_to_string(node_id)
+            score = predictions[node_id]
+            if score > self.score_threshold:
+                rospy.loginfo('%s (score = %.5f)' % (human_string, score))
                 self._pub.publish(human_string)
 
     def main(self):
